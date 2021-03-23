31ed51f045353f2d52f74167101ebe9730ad4f49
ACK-J <60232273+ACK-J@users.noreply.github.com>
2021-03-22 19:29:47 -0700

    Partially fixed toggle

--- popup/display_switch.js
+++ popup/display_switch.js
@@ -1,23 +1,42 @@
 import { start, stop } from "../background.js";
 
-let enabled = true;
-
 function toggleEnabled(e) {
+  // Error check
   if (e === null){
     return;
   }
-  if (enabled){
+  // Check the storage to see the position of the toggle
+  let checkPosition = localStorage.getItem("check");
+  // If the function is on
+  if (checkPosition == "true"){
     stop();
     console.log("Stopped");
-    enabled = false;
-  }else{
+    localStorage.removeItem('check');
+    localStorage.setItem('check', false);
+  }else{  // If the function is off
     start();
     console.log("Started");
-    enabled = true;
+    localStorage.removeItem('check');
+    localStorage.setItem('check', true);
   }
 }
 
+function checkIfEnabled(){
+  // Check the storage to see the position of the toggle
+  let check = localStorage.getItem("check");
+  // Returns either true or false 
+  let position = document.getElementById("PortAuthorityCheckbox").checked;
+  // If the function of the addon and the toggle are mis-matched, click it
+  if (check != position){
+    document.getElementById("onOffSwitch").click();
+  }
+}
+
+
+checkIfEnabled();
+// Add an event listener to the switch
 document.getElementById("PortAuthoritySlidingSwitch").addEventListener("click", toggleEnabled);
+
 
 
 
