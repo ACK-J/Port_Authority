347f1119b10ad4e944ad6be5aff0303500d3d5e9
ACK-J <60232273+ACK-J@users.noreply.github.com>
2021-03-31 17:37:40 -0700

    Was missing ignore case flag for regex. Added CNAME detection for threat metrix scripts. fixed 10. port scanning bug and removed the hacky way.

--- background.js
+++ background.js
@@ -1,13 +1,10 @@
-localStorage.removeItem('check');
-localStorage.setItem('check', true);
-
 let badges = {};
 
 function onError(error) {
-  console.error(`Error: ${error}`);
+  console.error(error);
 }
 
-function notify(){
+function notifyPortScanning(){
   browser.notifications.create("port-scanning-notification", {
     "type": "basic",
     "iconUrl": browser.runtime.getURL("icons/logo-96.png"),
@@ -16,37 +13,65 @@ function notify(){
   });
 }
 
-function cancel(requestDetails) {
-    // Please note that the regex in https://regex101.com/r/DOPCdB/13/ is different from below, since below I needed to change \b -> \\b
-    var local_filter = new RegExp("\\b((http|https|wss|ws):\/\/127\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)|:\/\/0?10\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)|(http|https|wss|ws):\/\/localhost|(http|https|wss|ws):\/\/172\.(0?16|0?17|0?18|0?19|0?20|0?21|0?22|0?23|0?24|0?25|0?26|0?27|0?28|0?29|0?30|0?31)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)|(http|https|wss|ws):\/\/192\.168\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)|(http|https|wss|ws):\/\/169\.254\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)|(http|https|wss|ws):\/\/::1|(http|https|wss|ws):\/\/[fF][cCdD][0-9a-fA-F]{2}(?:[:][0-9a-fA-F]{0,4}){0,7}|(http|https|wss|ws):\/\/[fF][eE][89aAbB][0-9a-fA-F](?:[:][0-9a-fA-F]{0,4}){0,7})(?:\/([789]|1?[0-9]{2}))?\\b");
+function notifyThreatMetrix(){
+  browser.notifications.create("threatmetrix-notification", {
+    "type": "basic",
+    "iconUrl": browser.runtime.getURL("icons/logo-96.png"),
+    "title": "Port Authority blocked this site from selling your data!",
+    "message": "This site pays Lexis Nexis (Threat Metrix) and TRIED to collect 416 pieces of identifying information about you to sell"
+  });
+}
+
+
+async function cancel(requestDetails) {
+    console.log(requestDetails.url);
+    // Please note that the regex in https://regex101.com/r/DOPCdB/14/ is different from below, since below I needed to change \b -> \\b
+    let local_filter = new RegExp("\\b(^(http|https|wss|ws|ftp|ftps):\/\/127\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)|^(http|https|wss|ws|ftp|ftps):\/\/(10)([.](25[0-5]|2[0-4][0-9]|1[0-9]{1,2}|[0-9]{1,2})){3}|^(http|https|wss|ws|ftp|ftps):\/\/localhost|^(http|https|wss|ws|ftp|ftps):\/\/172\.(0?16|0?17|0?18|0?19|0?20|0?21|0?22|0?23|0?24|0?25|0?26|0?27|0?28|0?29|0?30|0?31)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)|^(http|https|wss|ws|ftp|ftps):\/\/192\.168\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)|^(http|https|wss|ws|ftp|ftps):\/\/169\.254\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)|^(http|https|wss|ws|ftp|ftps):\/\/::1|^(http|https|wss|ws|ftp|ftps):\/\/[fF][cCdD][0-9a-fA-F]{2}(?:[:][0-9a-fA-F]{0,4}){0,7}|^(http|https|wss|ws|ftp|ftps):\/\/[fF][eE][89aAbB][0-9a-fA-F](?:[:][0-9a-fA-F]{0,4}){0,7})(?:\/([789]|1?[0-9]{2}))?\\b", "i");
+    // Create a regex to find all sub-domains for online-metrix.net
+    let thm = new RegExp("online-metrix\.net$", "i");
     // Error check
-    if (requestDetails.originUrl === null || requestDetails.url === null){
-        return {cancel: false};
+    //if (requestDetails.originUrl == null || requestDetails.originUrl == undefined  || requestDetails.url == null || requestDetails.url == undefined){
+    //    return {cancel: false};
+    //}
+
+    // Make sure we are not searching the CNAME of local addresses
+    if (requestDetails.url.search(local_filter) !== 0){
+        // Parse the URL
+        let url = new URL(requestDetails.url);
+        // Send a request to get the CNAME of the webrequest
+        let resolving = await browser.dns.resolve(url.host, ["canonical_name"]);
+        // If the CNAME redirects to a online-metrix.net domain -> Block
+        if (resolving.canonicalName.search(thm) !== -1){
+            notifyThreatMetrix();
+            return {cancel: true};
+        }
     }
+
+
     // Check if the network request is going to a local address
     // search should return a 0 for the 0th index of the string
     // if a match is further down the URL, it is probably a FP
     if (requestDetails.url.search(local_filter) === 0){ 
-	    // Check if the current website visited is a local address
-	    if (requestDetails.originUrl.search(local_filter) !== 0){
+        // Check if the current website visited is a local address
+        if (requestDetails.originUrl.search(local_filter) !== 0){
                 let tabId = requestDetails.tabId;
                 increaseBadged(requestDetails);
                 if (badges[tabId].alerted == 0){
-                    notify();
+                    notifyPortScanning();
                     badges[tabId].alerted += 1;
                 }
-	        return {cancel: true};
-	    }
+            return {cancel: true};
+        }
     }
     // Dont block sites that don't alert the detection
     return {cancel: false};
-}
+} // end cancel()
 
 export function start() {
     //Add event listener
     browser.webRequest.onBeforeRequest.addListener(
         cancel,
-        {urls: ["*://*/*"]}, // Match all HTTP, HTTPS and WebSocket URLs.
+        {urls: ["<all_urls>"]}, // Match all HTTP, HTTPS and WebSocket URLs.
         ["blocking"] // if cancel() returns true block the request.
     );
 }
