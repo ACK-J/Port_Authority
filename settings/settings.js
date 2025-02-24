4c71068639dd81ccb242efd836c57259dd02dec0
Matthew Sundberg <matthew@mws.is>
2025-02-23 17:24:56 -0800

    Refactored settings page js to use atomic modifications and better encapsulated form action handling

--- settings/settings.js
+++ settings/settings.js
@@ -1,76 +1,78 @@
-import { getItemFromLocal, setItemInLocal, modifyItemInLocal } from "../BrowserStorageManager.js";
+import { getItemFromLocal, modifyItemInLocal } from "../BrowserStorageManager.js";
 
-async function handleClick(e) {
-    if (e.target.dataset.action === "removeDomain" && e.target.dataset.domain) {
-        const deleting_domain = e.target.dataset.domain;
-
-        await modifyItemInLocal("allowed_domain_list", [],
-            (list) => list.filter(
-                (domain) => domain !== deleting_domain
-            ));
-
-        load_allowed_domains();
-    }
-}
-
+let remove_buttons_event_controller;
 async function load_allowed_domains() {
-    // Remove the current event listener to avoid multiple event listeners
-    if (
-        document.querySelector("button").addEventListener("click", handleClick)
-    ) {
-        document
-            .querySelector("button")
-            .removeEventListener("click", handleClick);
-    }
+    // Remove all of the stale listeners (might not need this since calling `replaceChildren` should kill all the children's listeners, but better safe than sorry)
+    if (remove_buttons_event_controller) remove_buttons_event_controller.abort();
 
-    document.querySelector("button").addEventListener("click", handleClick);
+    // Make a new AbortController for all of the fresh buttons
+    remove_buttons_event_controller = new AbortController();
+    const signal = remove_buttons_event_controller.signal;
+
     const allowedDomainsList = await getItemFromLocal(
         "allowed_domain_list",
         []
     );
 
-    const domainDomElements = allowedDomainsList.map((domain) => {
-        const button = document.createElement("button");
-        button.innerHTML = "Remove";
-        button.dataset.domain = domain;
-        button.dataset.action = "removeDomain";
+    // Results in a list of `<li>domain.com <button onclick="remove & refresh UI">Remove</button></li>`
+    const domainListDomElements = allowedDomainsList.map((domain) => {
+        const listItem = document.createElement("li");
+        listItem.innerText = domain + " ";
 
-        return `<li>${domain} ${button.outerHTML}</li>`;
+        const button = document.createElement("button");
+        button.innerText = "Remove";
+        button.addEventListener("click", async () => {
+            // Remove the current domain from the list
+            await modifyItemInLocal("allowed_domain_list", [],
+                (list) => list.filter(
+                    (d) => d !== domain
+                ));
+
+            // Refresh the domain list displayed
+            load_allowed_domains();
+        }, { signal }); // By triggering `remove_buttons_event_controller.abort()`, all buttons with this signal passed will have their listeners removed
+        listItem.appendChild(button);
+
+        return listItem;
     });
 
-    const allowDomainsListDomElement = document.getElementById(
-        "allowedDomainsListID"
-    );
-    allowDomainsListDomElement.innerHTML = domainDomElements.join("");
-    allowDomainsListDomElement.addEventListener("click", handleClick);
+    // Override the old list with the new contents
+    const listContainerElement = document.getElementById("allowedDomainsListID");
+    listContainerElement.replaceChildren(...domainListDomElements);
 }
 
-async function saveOptions(e) {
-    const allowed_domains_list = await getItemFromLocal(
-        "allowed_domain_list",
-        []
-    );
+function extractURLHost(text) {
+    let url = text + ""; // cast to string (is this needed?)
 
     // We don't actually care about the protocol as we only compare url.host
     // But the URL object will fail to create if no protocol is provided
-    let url = e.target[0].value + "";
-    if (url.slice(0, 4) != "http") {
+    if (url.slice(0, 4) !== "http") {
         url = "https://" + url;
     }
+    const newUrl = new URL(url);
+    return newUrl.host;
+}
+
+async function saveOptions(e) {
+    let url;
     try {
-        const newUrl = new URL(url);
-        const newUrlHost = newUrl.host;
-        if (allowed_domains_list.indexOf(newUrlHost) !== -1) {
-            alert("This domain is already in the list.");
-            return;
-        }
-        allowed_domains_list.push(newUrlHost);
-        await setItemInLocal("allowed_domain_list", allowed_domains_list);
-    } catch (error) {
+        url = extractURLHost(e.target[0].value);
+    } catch(error) {
         console.error(error);
         alert("Please enter a valid domain.");
         return;
     }
+
+    await modifyItemInLocal("allowed_domain_list", [],
+        (list) => {
+            // Only update the list if it's a new member
+            if (!list.includes(url)) {
+                return list.concat(url);
+            } else {
+                alert("This domain is already in the list.");
+                return list;
+            }
+        });
 }
 
 load_allowed_domains();
