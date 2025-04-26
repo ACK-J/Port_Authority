031596dcb1279c63a7cd42718abba7f96f68a522
mwsundberg <gitcommits@mws.is>
2025-04-26 05:03:35 -0700

    Rewrite of popup data rendering JS
    - Separates host and portscans blocking sections, conditionally hiding them when no blocking has occurred
    - Reordered portscan blocking ahead of tracking script blocking due to higher prevalence
    - Uses only CSS for collapsing/showing the ports with a `:has(input:checked)` CSS selector trick
    - Vastly simplifies HTML structure due to no longer needing to match bootstrap formatting
    - Currently lacks animation when expanding the ports list. This is a compromise which could be fixed with JS (excessive and annoying) or with CSS once Firefox starts supporting `interpolate-size: allow-keywords`

--- popup/PopupUI.js
+++ popup/PopupUI.js
@@ -1,5 +1,6 @@
 import { getItemFromLocal } from "../global/BrowserStorageManager.js";
 import { getActiveTabId } from "../global/browserActions.js";
+import { createElement } from "../global/domUtils.js";
 
 /**
  * Data fetching only, separated from rendering logic
@@ -14,207 +15,129 @@ async function fetch_tabs_blocking_data(data_type) {
     return all_tabs_data[tab_id];
 }
 
-const SECTION_HEADER_ELEMENT = "h2";
-
 /**
- * Applies an object of variable_name: variable_value as attributes to a provided DOM element.
- *
- * @param {element} element The element to add data attributes to
- * @param {object} attributes An object of all values to add
+ * An item in the popup display for "Blocked Port Scans"
+ * @param {string} host The LAN hostname/IP that was accessed
+ * @param {string[]} ports Which port(s) were scanned
+ * @returns {Element} An element that represents a portscanned domain and the ports that were scanned, structured as follows
+ * 
+ * ```html
+ * <li class="blocked-host-item">
+ *     <span class="host">{host}</span>
+ *     <label class="ports-expansion-toggle unselectable">
+ *         <input type="checkbox">
+ *         View Ports
+ *     </label>
+ *     <ul class="ports-expansion-target">
+ *         <li class="port">{ports[0]}</li>
+ *         {<li class="port">{ports[1]}</li>...}
+ *     </ul>
+ * </li>
+ * ```
  */
-const setAttributesOnElement = (element, attributes) => {
-    // Grab the list of attribute names to add
-    const attribute_names = Object.keys(attributes);
+function blocked_ports_item(host, ports) {
+    const container = createElement("li", {class: "blocked-host-item"});
 
-    // For each attribute, add the name and value to the elements attributes
-    for (let i = 0; i < attribute_names.length; i++) {
-        const attribute = attribute_names[i];
-        element.setAttribute(attribute, attributes[attribute]);
+    // The host/domain itself
+    const host_span = createElement("span", {class: "host"}, host);
+    container.appendChild(host_span);
+
+    // No ports case: return early and warn
+    if (ports.length === 0) {
+        console.warn("No port supplied when rendering blocked portscans for '" + host + "'");
+
+        return container;
     }
-};
 
-function buildSectionWrapper() {
-    const section_wrapper = document.createElement("div");
-    section_wrapper.classList.add("col-12", "d-flex", "flex-column");
-    return section_wrapper;
-}
-
-/**
- * Generates markup for a bootstrap collapse element.
- * The collapse has a title with a btn-link on the opposite side
- * <div>
- *  <div class="d-flex justify-content-between">
- *      <h6>collapse_title</h5>
- *      <button>toggle_text</button>
- *  </div>
- * </div>
- *
- * @param {string} data_target ID of the collapse element used for the toggles data target.
- * @param {string} collapse_title Title of the collapse element
- * @param {string} toggle_text Text for the toggle button
- * @returns {element} A collapse Wrapper with a button to toggle the collapse
- */
-function buildCollapseWrapperAndToggle(
-    data_target,
-    collapse_title,
-    toggle_text
-) {
-    // Wrapper to hold the title, toggle, and collapse element
-    const collapse_wrapper = document.createElement("div");
-    const title_toggle_wrapper = document.createElement("div");
-
-    // Wrapper to hold the title and toggle button
-    title_toggle_wrapper.classList.add(
-        "d-flex",
-        "justify-content-between",
-        "align-items-center"
-    );
-
-    // Title of the collapse
-    const title_element = document.createElement("h6");
-    title_element.innerText = collapse_title;
-    title_element.classList.add("bold-text");
-    title_toggle_wrapper.appendChild(title_element);
-
-    // Collpase toggle button
-    const collapse_toggle_button = document.createElement("button");
-
-    collapse_toggle_button.innerText = toggle_text;
-    const collapse_attributes = {
-        type: "button",
-        class: "btn btn-link",
-        "data-bs-target": `#${data_target}`,
-        "data-bs-toggle": "collapse",
-        "aria-expanded": false,
-        "aria-controls": data_target,
-    };
-    setAttributesOnElement(collapse_toggle_button, collapse_attributes);
-    title_toggle_wrapper.appendChild(collapse_toggle_button);
-
-    collapse_wrapper.appendChild(title_toggle_wrapper);
-
-    return collapse_wrapper;
-}
-
-/**
- * Displays a list of blocked ports in the popup UI.
- * Data is re-rendered each time the popup is opened.
- */
-async function updateBlockedPortsDisplay() {
-    const blocked_data_display = document.getElementById(
-        "blocked_data_display"
-    );
-    // Create a wrapper element to hold the header and list of blocked hosts
-    const all_ports_wrapper = buildSectionWrapper();
-
-    // Build the header/title element for this section
-    const all_ports_header = document.createElement(SECTION_HEADER_ELEMENT);
-    all_ports_header.innerText = "Blocked Port Scans:";
-    all_ports_header.classList.add("bold-text");
-
-    // Add the header to the blocked hosts wrapper element
-    all_ports_wrapper.appendChild(all_ports_header);
-
-    // Grab the blocked ports from the extensions local storage.
-    const blocked_ports = fetch_tabs_blocking_data("blocked_ports");
-
-    // Early return if no data
-    if (!blocked_ports) return;
-
-    const hosts = Object.keys(blocked_ports);
-
-    // build a tree for each host that was blocked
-    for (let i_host = 0; i_host < hosts.length; i_host++) {
-        // Build the wrapper for displaying the host name and ports blocked
-        const host = hosts[i_host];
-        const host_id = `host${i_host}`;
-        const host_wrapper = buildCollapseWrapperAndToggle(
-            host_id,
-            host,
+    /**"View Ports" toggle
+     * 
+     * Collapse/expand functionality is added with CSS.
+     * Wrapping `<label>` instead of placing it after the checkbox to avoid having to set a unique id on each input.
+     */
+    const expansion_toggle = createElement("label", {class: ["ports-expansion-toggle", "unselectable"]},
+        [
+            createElement("input", {type: "checkbox"}),
             "View Ports"
+        ]
+    );
+    container.appendChild(expansion_toggle);
+
+    // Expandable container for the ports list
+    const ports_ul = createElement("ul", {class: "ports-expansion-target"});
+    for (const port of ports) {
+        ports_ul.append(
+            createElement("li", {class: "port"}, port),
         );
+    }
+    container.appendChild(ports_ul);
 
-        // build the list of blocked ports then append it to the wrapper
-        const hosts_ul = document.createElement("div");
-        hosts_ul.id = host_id;
-        hosts_ul.classList.add("list-unstyled", "collapse");
+    // Item finally fully populated
+    return container;
+}
 
-        const ports = blocked_ports[hosts[i_host]];
+/**
+ * An item in the popup dispay for "Blocked Tracking Scripts"
+ * @param {string} host A threatmetrix domain that got blocked
+ * @returns {Element} A `<li>` wrapping the host string
+ */
+function blocked_hosts_item(host) {
+    return createElement("li", {}, host);
+}
 
-        // Add each port to the HTML
-        for (let i_port = 0; i_port < ports.length; i_port++) {
-            const port = ports[i_port];
-            const port_element = document.createElement("div");
-            port_element.innerText = port;
-            port_element.classList.add("ps-2");
-            hosts_ul.appendChild(port_element);
-        }
+// Populate `#blocked_ports`
+const blocked_ports_wrapper = document.getElementById("blocked_ports");
+const blocked_ports_contents = blocked_ports_wrapper.querySelector(".dropzone");
+async function load_blocked_ports(blocked_ports_object) {
+    // If not provided, fetch the data from storage
+    blocked_ports_object ??= await fetch_tabs_blocking_data("blocked_ports");
 
-        host_wrapper.appendChild(hosts_ul);
-        all_ports_wrapper.appendChild(host_wrapper);
+    // Clear stale contents, if any
+    blocked_ports_contents.replaceChildren();
+
+    // Early return, hiding wrapper if no data provided
+    if(!blocked_ports_object || Object.keys(blocked_ports_object).length === 0) {
+        blocked_ports_wrapper.setAttribute("hidden", "");
+        return;
     }
 
-    blocked_data_display.appendChild(all_ports_wrapper);
+    // Populate the list items
+    for(const host in blocked_ports_object) {
+        const item = blocked_ports_item(host, blocked_ports_object[host]);
 
-    // Append the header to the GUI
-    const blocked_data_display_ports = document.getElementById(
-        "blocked_data_display"
-    );
-    blocked_data_display_ports.appendChild(all_ports_wrapper);
+        blocked_ports_contents.appendChild(item);
+    };
+
+    // Unhide the container wrapper at end
+    blocked_ports_wrapper.removeAttribute("hidden");
 }
 
-async function updateBlockedHostsDisplay() {
-    // Create a wrapper element to hold the header and list of blocked hosts
-    const hosts_wrapper = buildSectionWrapper();
+// Populate `#blocked_hosts`
+const blocked_hosts_wrapper = document.getElementById("blocked_hosts");
+const blocked_hosts_contents = blocked_hosts_wrapper.querySelector('.dropzone');
+async function load_blocked_hosts(blocked_hosts_list) {
+    // If not provided, fetch the data from storage
+    blocked_hosts_list ??= await fetch_tabs_blocking_data("blocked_hosts");
 
-    // Build the header/title element for this section
-    const host_header = document.createElement(SECTION_HEADER_ELEMENT);
-    host_header.innerText = "Blocked Tracking Scripts:";
-    host_header.classList.add("bold-text");
+    // Clear stale contents, if any
+    blocked_hosts_contents.replaceChildren();
 
-    // Add the header to the blocked hosts wrapper element
-    hosts_wrapper.appendChild(host_header);
-
-    // create the UL element to hold all the blocked hosts
-    const hosts_ul = document.createElement("ul");
-    hosts_ul.classList.add("list-unstyled");
-
-    try {
-        const blocked_hosts = fetch_tabs_blocking_data("blocked_hosts") || [];
-
-        // Build a list of host names as li elements
-        for (let host = 0; host < blocked_hosts.length; host++) {
-            //Grab the host name
-            const host_name = blocked_hosts[host];
-
-            // Create the list element for the blocked host and set the text to the hosts name
-            const host_li = document.createElement("li");
-            host_li.classList.add("ps-2", "brand-text-color", "bold-text");
-            host_li.innerText = host_name;
-
-            // Add the list element to the hosts UL
-            hosts_ul.appendChild(host_li);
-        }
-    } catch (error) {
-        // Something went wrong, empty the ul to be safe
-        hosts_ul.innerText = "";
+    // Early return, hiding wrapper if no data provided
+    if(!blocked_hosts_list || blocked_hosts_list.length === 0) {
+        blocked_hosts_wrapper.setAttribute("hidden", "");
+        return;
     }
-    // Add the list of blocked hosts to the wrapper containing the section header
-    hosts_wrapper.appendChild(hosts_ul);
 
-    // Append the list of blocked hosts to the Popups blocked_data_display section
-    const blocked_data_display = document.getElementById(
-        "blocked_data_display"
-    );
-    blocked_data_display.appendChild(hosts_wrapper);
+    // Populate the list items
+    for(const host of blocked_hosts_list) {
+        const item = blocked_hosts_item(host);
+
+        blocked_hosts_contents.appendChild(item);
+    };
+
+    // Unhide the container wrapper at end
+    blocked_hosts_wrapper.removeAttribute("hidden");
 }
 
-// Helper function for calling all DOM-Modifying functions
-function buildDataMarkup() {
-    // Shows any and all hosts that attempted to connect to a tracking service
-    updateBlockedHostsDisplay();
-    // Shows any and all ports that were blocked from scanning. Ports are sorted based on host that attempted the port scan
-    updateBlockedPortsDisplay();
-}
-
-buildDataMarkup();
+// TODO live rerendering on data change, could use storage event coordinating as discussed in issue #50: https://github.com/ACK-J/Port_Authority/issues/50
+load_blocked_ports();
+load_blocked_hosts();
